3f0a13432e4ab060af5f1f5885b7f5fe3df7d89d
Oduvanchick111 <pboltinsky@yandex.ru>
2025-10-22 00:11:33 +0300

    checkstyle

--- mainService/src/main/java/ewm/API/adminAPI/compilations/AdminCompilationsServiceImpl.java
+++ mainService/src/main/java/ewm/API/adminAPI/compilations/AdminCompilationsServiceImpl.java
@@ -1,6 +1,5 @@
 package ewm.API.adminAPI.compilations;
 
-import ewm.models.apiError.model.ValidateException;
 import lombok.AllArgsConstructor;
 import ewm.models.apiError.model.ConflictException;
 import ewm.models.apiError.model.NotFoundException;
