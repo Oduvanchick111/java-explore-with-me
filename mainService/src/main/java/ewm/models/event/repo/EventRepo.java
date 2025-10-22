2de76ce043659e6d4b7f5b143b27041cd90cc1d5
Oduvanchick111 <pboltinsky@yandex.ru>
2025-10-23 00:46:31 +0300

    )

--- mainService/src/main/java/ewm/models/event/repo/EventRepo.java
+++ mainService/src/main/java/ewm/models/event/repo/EventRepo.java
@@ -19,11 +19,12 @@ public interface EventRepo extends JpaRepository<Event, Long> {
             WHERE e.eventState = 'PUBLISHED'
               AND (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%'))
                    OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%')))
-              AND (:paid IS NULL OR e.paid = :paid)
               AND (:categories IS NULL OR e.category.id IN :categories)
+              AND (:paid IS NULL OR e.paid = :paid)
               AND e.eventDate >= COALESCE(:rangeStart, CURRENT_TIMESTAMP)
               AND e.eventDate <= COALESCE(:rangeEnd, e.eventDate)
               AND (:onlyAvailable = false OR e.participantLimit = 0 OR e.confirmedRequests < e.participantLimit)
+            ORDER BY e.eventDate
             """)
     Page<Event> findPublicEvents(@Param("text") String text,
                                  @Param("categories") List<Long> categories,
